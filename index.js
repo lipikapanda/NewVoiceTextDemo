e2d563a07d1f9f71056680131647134f212f1249
unknown <lipikapanda88@gmail.com>
2020-03-07 22:49:37 +0530

    Signed-off-by: unknown <lipikapanda88@gmail.com>

--- index.js
+++ index.js
@@ -20,42 +20,12 @@ restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
 
-  var param = "AEs";
-  var inDrugName = "humira"
-  var inAction = "year"
-  var inDay = "-2"
+
 
   const promise1 = new Promise(function(resolve, reject) {
-    resolve("hello");
+    resolve(msg);
   });
 
-  superagent.get('http://103.224.243.38/3Analytics/WS_VoiceResult.asmx/GetDrugDetails')
-      .query({ DrugName: inDrugName, szAction:inAction, szDay:inDay })
-      .end((err, res) => {
-
-        var result = res.text;
-
-        const arrList = result.replace("[{","}]").split("}]")[1].split(",");
-        var strDrugName = arrList[0].split(":")[1];
-        var strAE = arrList[1].split(":")[1];
-        var strSerious = arrList[2].split(":")[1];
-        var strDeath = arrList[3].split(":")[1];
-
-        switch(param){
-          case "death":
-            speech1 = strDeath + " death cases have been reported for drug " + strDrugName + " in last 5 years";
-            break;
-
-          case "serious":
-            speech1 = strSerious + " serious cases have been reported for drug " + strDrugName + " in last 5 years";
-            break;
-
-          case "AEs":
-            speech1 = strAE + " AE's have been reported for drug " + strDrugName + " in last 5 years";
-            break;
-        }
-
-      });
 
 
   var speech =
@@ -89,6 +59,43 @@ restService.post("/echo", function(req, res) {
     source: "webhook-echo-sample"
   });
 });
+
+function msg(){
+
+  var param = "AEs";
+  var inDrugName = "humira"
+  var inAction = "year"
+  var inDay = "-2"
+
+  superagent.get('http://103.224.243.38/3Analytics/WS_VoiceResult.asmx/GetDrugDetails')
+      .query({ DrugName: inDrugName, szAction:inAction, szDay:inDay })
+      .end((err, res) => {
+
+        var result = res.text;
+
+        const arrList = result.replace("[{","}]").split("}]")[1].split(",");
+        var strDrugName = arrList[0].split(":")[1];
+        var strAE = arrList[1].split(":")[1];
+        var strSerious = arrList[2].split(":")[1];
+        var strDeath = arrList[3].split(":")[1];
+
+        switch(param){
+          case "death":
+            speech1 = strDeath + " death cases have been reported for drug " + strDrugName + " in last 5 years";
+            break;
+
+          case "serious":
+            speech1 = strSerious + " serious cases have been reported for drug " + strDrugName + " in last 5 years";
+            break;
+
+          case "AEs":
+            speech1 = strAE + " AE's have been reported for drug " + strDrugName + " in last 5 years";
+            break;
+        }
+
+      });
+  return speech1;
+}
 
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
