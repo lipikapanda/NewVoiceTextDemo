b07092775f86257262214088d32fd6041887c813
unknown <lipikapanda88@gmail.com>
2020-03-07 21:41:05 +0530

    Signed-off-by: unknown <lipikapanda88@gmail.com>

--- index.js
+++ index.js
@@ -28,8 +28,31 @@ restService.post("/echo", function(req, res) {
   superagent.get('http://103.224.243.38/3Analytics/WS_VoiceResult.asmx/GetDrugDetails')
       .query({ DrugName: inDrugName, szAction:inAction, szDay:inDay })
       .end((err, res) => {
+
+        var result = res.text;
+
+        const arrList = result.replace("[{","}]").split("}]")[1].split(",");
+        var strDrugName = arrList[0].split(":")[1];
+        var strAE = arrList[1].split(":")[1];
+        var strSerious = arrList[2].split(":")[1];
+        var strDeath = arrList[3].split(":")[1];
+
+        switch(param){
+          case "death":
+            speech1 = strDeath + " death cases have been reported for drug " + strDrugName + " in last 2 years";
+            break;
+
+          case "serious":
+            speech1 = strSerious + " serious cases have been reported for drug " + strDrugName + " in last 2 years";
+            break;
+
+          case "AEs":
+            speech1 = strAE + " AE's have been reported for drug " + strDrugName + " in last 2 years";
+            break;
+        }
+
       });
-      
+
 
   var speech =
       req.body.queryResult &&
